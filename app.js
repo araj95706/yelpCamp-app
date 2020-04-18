a4ab7a774f2f55a6825934bd58f1c7230fc07aba
ashish-l00l <araj95706@gmail.com>
2020-04-18 13:45:00 +0530

    update

--- app.js
+++ app.js
@@ -3,48 +3,41 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp-camp-app");
-mongoose.connect("uri", { useNewUrlParser: true });
-mongoose.connect("uri", { useFindAndModify: false });
-mongoose.connect("uri", { useCreateIndex: true });
-mongoose.connect("uri", { useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/test1", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 var campGroundsSchema = new mongoose.Schema({
-    name: String,
-    image: String
+  name: String,
+  image: String,
 });
 
 var campGrounds = mongoose.model("campGrounds", campGroundsSchema);
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("landing");
+  res.render("landing");
 });
 
 app.get("/campGrounds", (req, res) => {
-    res.render("campGrounds", {campGrounds: campGrounds});
+  res.render("campGrounds", { campGrounds: campGrounds });
 });
 
-app.post("/campGrounds", (req, res) => {    
-    var name = req.body.name;
-    var image = req.body.image;
-    
-    campGrounds.create({name: name, image: image});
+app.post("/campGrounds", (req, res) => {
+  var name1 = req.body.name;
+  var image1 = req.body.image;
+  campGrounds.insertMany({ name: name1, image: image1 });
 
-    res.redirect("/campGrounds");
+  res.redirect("/campGrounds");
 });
 
 app.get("/campGrounds/new", (req, res) => {
-    res.render("new");
+  res.render("new");
 });
 
-var server = app.listen('4646', () => {
-    console.log("the sever has been started.");
+var server = app.listen("4444", () => {
+  console.log("the sever has been started.");
 });
